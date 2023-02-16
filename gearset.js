aefd46529e3a006bbdf0b4b6455530f72dc34275
howshekilledit <morganhg@uic.edu>
2023-02-16 13:38:29 -0800

    decimal  gearset two places working pretty well

--- gearset.js
+++ gearset.js
@@ -11,7 +11,7 @@ class gear {
         this.t = 0; //highlighted tooth
         this.f = 0; //rotating frames elapsed
         this.fr = 10; //rotating frames per tooth
-        this.rotate_amt = this.angle/this.fr; //amount to rotate gear on each frame
+        this.rotate_amt = this.angle / this.fr; //amount to rotate gear on each frame
         //outer gear points
         for (var i = 0; i < n; i++) {
             //outer gear points
@@ -20,18 +20,18 @@ class gear {
         }
         this.pts.push(this.pts[0]); //close the polygon
         this.inner_pts = []; //array of inner points
-        let inner_radius = r -th*1.5;
+        let inner_radius = r - th * 1.5;
         for (var i = 0; i < n; i++) {
-            this.inner_pts.push(pt_on_round(inner_radius, i * this.angle , ctr));
+            this.inner_pts.push(pt_on_round(inner_radius, i * this.angle, ctr));
             this.inner_pts.push(pt_on_round(inner_radius / this.m, i * this.angle + this.angle / 2, ctr)); //inner points
         }
         this.inner_pts.push(this.inner_pts[0]); //close the polygon
 
     }
-    draw_svg() {
-        this.rotation = 0; 
+    draw_svg(clr = '#000000') {
+        this.rotation = 0;
         //generate random color
-        this.color = color(random(255), random(255), random(255));
+        this.color = clr;
         var gearpts = this.pts.map(pt => [pt.x, pt.y]);
         var innerpts = this.inner_pts.map(pt => [pt.x, pt.y]);
         this.svg_gear = cnvs.group();
@@ -44,19 +44,19 @@ class gear {
         this.svg_gear = cnvs.group().add(this.svg_gear);
         this.lbls = []; //array of labels
         let exp = log(floor(this.pts.length / 2)) / log(n) - 1; //get exponent of gear
-       
+
         for (let [i, pt] of this.pts.entries()) {
             if (i % 2 == 0 && i < this.pts.length - 1) {
                 let index = floor(i / 2);
                 //change direction of numbering based on wheel direction
-                let lbl_txt; 
-                if (this.rotate_amt < 0) {
-                    lbl_txt = floor(index / (n ** exp));
-                } else {
-                    lbl_txt = floor((n ** exp - index / (n ** exp))%n);
-                }
+                let lbl_txt;
+                // if (this.rotate_amt < 0) {
+                lbl_txt = floor(index / (n ** exp));
+                // } else {
+                //     lbl_txt = floor((n ** exp - index / (n ** exp))%n);
+                // }
                 let coord = pt_on_round(this.r - this.th, index * this.angle, this.ctr);
-                let lbl = cnvs.text(lbl_txt).move(coord.x - font_size / 3, coord.y - font_size / 2).font({ size: font_size, family: 'Monospace', anchor: 'left', leading: '0em'});
+                let lbl = cnvs.text(lbl_txt).move(coord.x - font_size / 3, coord.y - font_size / 2).font({ size: font_size, family: 'Monospace', anchor: 'left', leading: '0em' });
                 lbl.rotate(index * this.angle + 180);
                 this.lbls.push(lbl);
                 //let ccl = cnvs.circle(6).move(coord.x-3, coord.y-3);
@@ -69,38 +69,23 @@ class gear {
         this.svg_gear.rotate(angle);
         if (update) {
             this.rotation = (this.rotation + angle) % 360;
-            this.t = (this.n-floor(this.rotation/this.angle))%this.n;
-            // if(this.t < 0){
-            //     this.t = (this.n - this.t)%this.n;
-            // } else {
-            //     this.t = (this.t)%this.n;
-            // }
+
             this.f++; //increment frame counter
-            if(this.f%this.fr == 0){ //if a whole tooth has been rotated
-                if (this.rotate_amt < 0) {
-                    this.t-=1;
-                } else {
-                    this.t+=1; 
-                }
-                if(this.t < 0){
-                    this.t = (this.n - this.t)%this.n;
-                } else {
-                    this.t = (this.t)%this.n;
-                }
-        
+            if (this.f % this.fr == 0) { //if a whole tooth has been rotated
+                this.t = (this.t + 1) % this.n
                 //this.t = this.t%this.n;
                 this.shift_colored_lbl();
             }
 
+
         }
-      
-        //if(this.rotation % this.angle == 0){
-            
-        //}
+
     }
-    shift_colored_lbl(){ //shift the colored label to the current tooth
-        this.lbls.map(lbl => lbl.font({fill:'#000'}));
-        this.lbls[this.t].font(({fill:'#f00'}));
+
+
+    shift_colored_lbl() { //shift the colored label to the current tooth
+        this.lbls.map(lbl => lbl.font({ fill: '#000' }));
+        this.lbls[this.t].font(({ fill: '#f00' }));
         this.focused_lbl = this.lbls[this.t].text();
         // let index = floor((this.rotation + this.angle/4) / this.angle);
         // if (index < 0){
@@ -112,19 +97,19 @@ class gear {
         // lbl.font({fill:'#f00'});
         // this.focused_lbl = lbl.text();    
     }
-    draw_focus_window(){ //draw circle around apex tooh of gear
+    draw_focus_window() { //draw circle around apex tooh of gear
         let window_ctr = pt_on_round(this.r - this.th, this.rotation, this.ctr);
         this.window_ctr = window_ctr;
-        let window_diam = min(this.th*2, 10*this.r/this.n);
+        let window_diam = min(this.th * 2, 10 * this.r / this.n);
         this.svg_window = cnvs.circle(window_diam).move(
-            window_ctr.x-window_diam/2, window_ctr.y-window_diam/2).fill('none').stroke({width:1, color:'#f00'});
+            window_ctr.x - window_diam / 2, window_ctr.y - window_diam / 2).fill('none').stroke({ width: 1, color: '#f00' });
         //this.svg_gear.add(window);
 
     }
 }
 
 
-class gearset { 
+class gearset {
     constructor(base, n_digits, max_r = 150,
         th = 20, offset = createVector(th, th), first = createVector(1000, 300)) {
         this.base = base;
@@ -133,24 +118,24 @@ class gearset {
         this.th = th; //thickness of gears
         let x = first.x; //x coordinate of first gear
         let y = first.y; //y coordinate of first gear
-        let r = max_r; 
+        let r = max_r;
         for (var i = n_digits; i >= 1; i--) {
             let n = base ** i;
-            if(i < n_digits){
-                r/= base/2;
-                r-=th; 
-                x+=r+2*th;
-        
+            if (i < n_digits) {
+                r /= base / 2;
+                r -= th;
+                x += r + 2 * th;
             }
 
-          
+
             //x -= r + offset.x;
-            this.gears.push(new gear(n, createVector(x, y), r, th));
-            x-=r;
-       
+            let next_gear = new gear(n, createVector(x, y), r, th);
+            x -= r;
+
             // if (i % 2) {
-            //     this.gears[i - 1].rotate_amt *= -1;
-            // }
+            next_gear.rotate_amt *= -1;
+            //}
+            this.gears.push(next_gear);
             //x -= r - 1;
             if (i % 2) {
                 y += offset.y;
@@ -161,10 +146,10 @@ class gearset {
     }
     draw_svg() {
         for (let g of this.gears) {
-
             g.draw_svg();
         }
     }
+
     label_svg() {
         for (let g of this.gears) {
             g.label_svg(this.base);
@@ -186,21 +171,21 @@ class gearset {
     getGear() {
         return this.gear;
     }
-    delete_svg(){
-        for (let g of this.gears){
+    delete_svg() {
+        for (let g of this.gears) {
             g.svg_gear.remove();
-            g.svg_window.remove();
+            //g.svg_window.remove();
         }
     }
-    draw_focus_windows(){
-        for (let g of this.gears){
+    draw_focus_windows() {
+        for (let g of this.gears) {
             g.draw_focus_window();
         }
     }
-    get_output(){
+    get_output() {
         let output = '';
-        for (let g of this.gears){
-            output  = g.focused_lbl + output ;
+        for (let g of this.gears) {
+            output = output + g.focused_lbl;
         }
         return output;
     }
--- sketch.js
+++ sketch.js
@@ -15,19 +15,19 @@ function setup(){
   angleMode(DEGREES);
   //noLoop(); 
   frameRate(10);
-  ten_gearset = new gearset(10,2, 600, 40, createVector(0, 5), createVector(windowWidth - 300, windowHeight/2));
+  ten_gearset = new gearset(10,2, 600, 40, createVector(15, 0), createVector(windowWidth - 300, windowHeight/2));
 
  
-  bin_gearset = new gearset(2, 5, 250, 20, createVector(-10, 20), createVector(300, windowHeight/2));
+  bin_gearset = new gearset(2, 3, 250, 20, createVector(-10, 20), createVector(300, windowHeight/2));
   set = draw_set(ten_gearset);
   //create buttons to toggle between gearsets
   let btn = createButton('Toggle Gearset');
   btn.position(10, 10);
   btn.mousePressed(function(){
     if(set == bin_gearset){
-      set = draw_set(bin_gearset, bin_gearset);
+      set = draw_set(ten_gearset, bin_gearset);
     } else {
-      set = draw_set(ten_gearset, ten_gearset);
+      set = draw_set(bin_gearset, ten_gearset);
     }
   }); 
   output_text = cnvs.text(' ').move(10, 50).font({size: 20, family: 'Helvetica'});
@@ -38,9 +38,9 @@ function setup(){
 }
 
 function draw(){
-  if(frameCount %3 == 0){
+  //if(frameCount %3 == 0){
     set.rotate_svg();
-  }
+  //}
   let output = set.get_output();
   output_text.text(output);
   
@@ -57,7 +57,7 @@ function draw_set(set, other_set = false){
   } 
   set.draw_svg();
   set.label_svg();
- // set.rotate_svg(270, false);
+  set.rotate_svg(180, false);
   //set.draw_focus_windows(); 
   return set; 
 }
