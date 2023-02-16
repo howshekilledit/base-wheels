66a57a36dff381d501d49bdcc90a7a0296c06935
howshekilledit <morganhg@uic.edu>
2023-02-16 13:48:52 -0800

    remove button

--- sketch.js
+++ sketch.js
@@ -21,15 +21,15 @@ function setup(){
   bin_gearset = new gearset(2, 3, 250, 20, createVector(-10, 20), createVector(300, windowHeight/2));
   set = draw_set(ten_gearset);
   //create buttons to toggle between gearsets
-  let btn = createButton('Toggle Gearset');
-  btn.position(10, 10);
-  btn.mousePressed(function(){
-    if(set == bin_gearset){
-      set = draw_set(ten_gearset, bin_gearset);
-    } else {
-      set = draw_set(bin_gearset, ten_gearset);
-    }
-  }); 
+  // let btn = createButton('Toggle Gearset');
+  // btn.position(10, 10);
+  // btn.mousePressed(function(){
+  //   if(set == bin_gearset){
+  //     set = draw_set(ten_gearset, bin_gearset);
+  //   } else {
+  //     set = draw_set(bin_gearset, ten_gearset);
+  //   }
+  // }); 
   output_text = cnvs.text(' ').move(10, 50).font({size: 20, family: 'Helvetica'});
   for(g of set.gears){
     g.shift_colored_lbl();
